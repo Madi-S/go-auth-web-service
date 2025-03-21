48fc32a3a17cf5e722a056b983d3d35a6f26992c
Madi-S <khovansky99@gmail.com>
2025-03-21 22:29:55 +0500

    Config and its loader implemented

--- go/go.mod
+++ go/go.mod
@@ -5,10 +5,15 @@ go 1.23.5
 require github.com/Madi-S/go-auth-web-service v0.0.0-20250321163048-ed8aa49b41d7
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
 	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ go/internal/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string        `yaml:"env" env:"ENV" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env:"TOKEN_TTL" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port" env:"GRPC_PORT"`
+	Timeout time.Duration `yaml:"timeout" env:"GRPC_TIMEOUT"`
+}
+
+// Loads and returns config,
+// panics if fails to do so.
+func MustLoad() *Config {
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist on given path: " + configPath)
+	}
+
+	var config Config
+	if err := cleanenv.ReadConfig(configPath, config); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &config
+}
+
+// Fetches config path from cli flag or
+// environmental variable CONFIG_PATH.
+//
+// Priority: flag > env > default.
+func fetchConfigPath() string {
+	var res string = "./config/dev.yml"
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
